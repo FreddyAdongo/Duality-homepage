3f7879f59197c35645bce96465425a39a10b72ec
Freddy <46581440+FreddyisFreddy@users.noreply.github.com>
2020-04-11 15:33:08 -0400

    Add files via upload

---
+++ Duality/hover_effect.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion_effect'),
+    intensity: 0.2,
+    image1: './Assets/yellow.png',
+    image2: './Assets/goth.png',
+    displacementImage: './Assets/distortion.png',
+    imagesRatio: 380 / 300,
+  })
